0c6fff4fed072b932bbca0827e99a2f600a290c6
Lee-Jin-Hyeok <jinjin0816@dsm.hs.kr>
2020-11-16 08:12:06 +0900

    [UPDATE] SliceFilter

--- src/main/java/com/dsm/immigration/utils/filters/SlicePreFilter.java
+++ src/main/java/com/dsm/immigration/utils/filters/SlicePreFilter.java
@@ -42,6 +42,22 @@ public class SlicePreFilter extends ZuulFilter {
 //                request.setAttribute("baseUrl", "https://gangwon:8080");
 //            }
             request.setAttribute("baseUrl", "https://gangwon:8080");
+        } else if(uri.equals("/auth")) {
+            request.setAttribute("baseUrl", "https://gangwon:8080");
+        } else if (uri.equals("/diary-book")) {
+            request.setAttribute("baseUrl", "https://hamgyeong:8080");
+        } else if (uri.startsWith("/diary-book")) {
+            if (method.equals("GET")) {
+                request.setAttribute("baseUrl", "https://jeju:4008");
+            } else if (method.equals("POST")) {
+                request.setAttribute("baseUrl", "https://jeju:4008");
+            }
+        } else if(uri.equals("/user/diary-books")) {
+            request.setAttribute("baseUrl", "https://hwanghae:8891");
+        } else if(uri.equals("/diary-book/participant")) {
+            request.setAttribute("baseUrl", "https://pyeongan:8892");
+        } else {
+            request.setAttribute("baseUrl", "localhost:8891");
         }
 
         request.setAttribute("method", method);
