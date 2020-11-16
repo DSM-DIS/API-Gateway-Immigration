17facb6c3bd98b03600a0bd862ca30cf6d8d8d3a
Lee-Jin-Hyeok <jinjin0816@dsm.hs.kr>
2020-11-16 10:52:13 +0900

    [UPDATE] log

--- src/main/java/com/dsm/immigration/ImmigrationApplication.java
+++ src/main/java/com/dsm/immigration/ImmigrationApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ImmigrationApplication {
-
 	public static void main(String[] args) {
+		System.out.println("서버는 켜지냐1");
 		SpringApplication.run(ImmigrationApplication.class, args);
+		System.out.println("서버는 켜지냐2");
 	}
-
 }
