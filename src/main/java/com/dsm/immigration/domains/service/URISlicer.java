cbe415d7882b8b26623d2fcfcda3204398cfecb9
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-11-20 19:21:53 +0900

    dfasd

--- src/main/java/com/dsm/immigration/domains/service/URISlicer.java
+++ src/main/java/com/dsm/immigration/domains/service/URISlicer.java
@@ -3,13 +3,9 @@ package com.dsm.immigration.domains.service;
 public class URISlicer {
     public static String slice(String uri) {
         char[] chars = uri.toCharArray();
-        int count = 0;
         for(int i = 1 ; i < chars.length ; i++) {
             if(chars[i] == '/') {
-                count++;
-                if(count >= 2) {
-                    return uri.substring(i + 1);
-                }
+                return uri.substring(i + 1);
             }
         }
         return "/";
