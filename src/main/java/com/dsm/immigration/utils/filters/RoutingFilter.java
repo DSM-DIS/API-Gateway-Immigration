be7cd8942f958b71d355d01609d5658a4e977df6
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-12-06 14:47:20 +0900

    FINALLY

--- src/main/java/com/dsm/immigration/utils/filters/RoutingFilter.java
+++ src/main/java/com/dsm/immigration/utils/filters/RoutingFilter.java
@@ -50,12 +50,12 @@ public class RoutingFilter extends ZuulFilter {
                 .build();
 
         String uri = (String) request.getAttribute("uri");
-        String body = context.getResponseBody();
-        
+//        String body = context.getResponseBody();
+        String body = "{'number':'2417','name':'이진혁'}";
         System.out.println("baseUrl: " + baseUrl);
         System.out.println("uri : " + uri);
         System.out.println("userId : " + userId);
-        System.out.println("context.body : " + context.getResponseBody());
+        System.out.println("context.body : " + body);
 
         DiaryStoryRequestConnectionService service = retrofit.create(DiaryStoryRequestConnectionService.class);
         Response<String> response = null;
