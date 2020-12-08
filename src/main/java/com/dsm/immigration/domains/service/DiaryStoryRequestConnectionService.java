fea539e01fcf99bffede56295706b12b5fc085eb
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-12-08 22:16:36 +0900

    FINALLY

--- src/main/java/com/dsm/immigration/domains/service/DiaryStoryRequestConnectionService.java
+++ src/main/java/com/dsm/immigration/domains/service/DiaryStoryRequestConnectionService.java
@@ -7,22 +7,18 @@ import java.util.concurrent.Executor;
 
 public interface DiaryStoryRequestConnectionService {
     @Headers(value = {"accept: application/json", "content-type: application/json"})
-    @GET("/{uri}")
-    Call<String> get(@Path("uri") String uri, @Header("userId") String userId);
-
-//    @Headers(value = {"accept: application/json", "content-type: application/json"})
-//    @POST("/{uri}")
-//    Call<String> post(@Path("uri") String uri, @Header("userId") String userId, @Body String body);
+    @GET
+    Call<String> get(@Url String uri, @Header("userId") String userId);
 
     @Headers(value = {"accept: application/json", "content-type: application/json"})
     @POST
     Call<String> post(@Url String uri, @Header("userId") String userId, @Body String body);
 
     @Headers(value = {"accept: application/json", "content-type: application/json"})
-    @PATCH("/{uri}")
-    Call<String> patch(@Path("uri") String uri, @Header("userId") String userId, @Body String body);
+    @PATCH
+    Call<String> patch(@Url String uri, @Header("userId") String userId, @Body String body);
 
     @Headers(value = {"accept: application/json", "content-type: application/json"})
-    @DELETE("/{uri}")
-    Call<String> delete(@Path("uri") String uri, @Header("userId") String userId);
+    @DELETE
+    Call<String> delete(@Url String uri, @Header("userId") String userId);
 }
